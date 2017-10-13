c99658ef604aa1dc68ca762719db2cef008e6554
brittnibegale <brittnibegale@gmail.com>
2017-10-13 09:36:48 -0500

    add first files

---
+++ TrashCollector/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Address
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display (Name = "Street number and name")]
+        public string Street { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Required]
+        [Display (Name = "Zipcode")]
+        public string Zipcode { get; set; }
+
+    }
+}
---
+++ TrashCollector/Models/Day.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Day
+    {
+        [Key]
+        public int ID { get; set; }
+        public string DayOfPickUp { get; set; }
+        public double Cost { get; set; }
+    }
+}
